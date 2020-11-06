9e91f02f0a237a81d8eca82fccfbfc3e6cd13e7e
Mark G <markghaida@gmail.com>
2020-11-06 19:49:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -228,7 +228,7 @@ def big_shoe_rebounds
         game_hash[key][:players].each do |player|
                    binding.pry
            if player[:shoe] == player[:shoe]
-              new_array << shoe[:shoe]
+              new_array << player[:shoe]
            end
             #binding.pry
         end
